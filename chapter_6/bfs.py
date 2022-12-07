bcb18c08fb02a8ee84b750db80034c9472fac543
Lucas Mirachi <lucas.mirachi@gmail.com>
2022-12-07 12:12:50 -0300

    style: added code comments to bfs.py

--- chapter_6/bfs.py
+++ chapter_6/bfs.py
@@ -6,17 +6,17 @@ def person_is_seller(name):
 def search(name):
     search_line = deque()
     search_line += grafo[name]
-    verified = []
+    verified = [] #This vector is the way that we mantain registered the names that were already verified 
     while search_line:
-        person = search_line.popleft()
-        if not person in verified:
+        person = search_line.popleft() #takes in the first person on the line
+        if not person in verified: #only verifiy the names of the persons who were not verified before
             if person_is_seller(person):
                 print (person + " is an mango seller!")
                 return True
             else:
-                search_line += grafo[person]
-                verified.append(person)
-    return False
+                search_line += grafo[person] #add all the "frieds" of this person to the list to be verified
+                verified.append(person) #mark the person as verified
+    return False #if you got here, it means that no person on the line is a mango seller
 
 grafo = {}
 grafo["you"] = ["alice", "bob", "claire"]
